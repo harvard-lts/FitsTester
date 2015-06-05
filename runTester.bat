28b51a9de0c888d6604bb2fbbbc13155d161754e
dbirtwellharvard <david_birtwell@harvard.edu>
2015-06-05 15:01:17 -0400

    revised the batch file

--- runTester.bat
+++ runTester.bat
@@ -3,10 +3,10 @@ REM This is the Batch file to run the FITS Test Application
 REM
 ECHO OFF
 set TEMP_CLASSPATH=./FitsTester-1.0.jar
-set TEMP_CLASSPATH=%TEMP_CLASSPATH%;test_libs/groovy-all-2.3.10.jar
-set TEMP_CLASSPATH=%TEMP_CLASSPATH%;test_libs/commons-io-2.4.jar
-set TEMP_CLASSPATH=%TEMP_CLASSPATH%;test_libs/log4j-1.2.11.jar
-set TEMP_CLASSPATH=%TEMP_CLASSPATH%;test_libs/xmlunit-1.4.jar
+set TEMP_CLASSPATH=%TEMP_CLASSPATH%;lib/groovy-all-2.3.10.jar
+set TEMP_CLASSPATH=%TEMP_CLASSPATH%;lib/commons-io-2.4.jar
+set TEMP_CLASSPATH=%TEMP_CLASSPATH%;lib/log4j-1.2.11.jar
+set TEMP_CLASSPATH=%TEMP_CLASSPATH%;lib/xmlunit-1.4.jar
 ECHO ON
 ECHO %TEMP_CLASSPATH%
 
